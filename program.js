d31b9ab92b676c4c4b527be8c8a987857bece5f0
Vladimir Galkin <vladgalkin@gmail.com>
2016-02-01 17:42:06 +0200

    Use generators to estimate PI value using Monte-Carlo method

--- program.js
+++ program.js
@@ -1,38 +1,55 @@
-var NumberOfTrials = 10000000;
-
-var SquareSideSize = 100;
+var SquareSideSize = 1;
 
 var CircleRadius = SquareSideSize;
+
+function estimatePi(squareSize) {
+    var numOfPoints         = 0;
+    var numOfPointsInCircle = 0;
+    var stepGenerator       = step(squareSize, generateRandomPoints(squareSize));
+
+    while (true) {
+        for (var point of take(stepGenerator, 100)) {
+            ++numOfPoints;
+
+            if (point.isInCircle) {
+                ++numOfPointsInCircle;
+            }
+        }
+
+        var estimatedPiValue = (numOfPointsInCircle / numOfPoints) * 4;
+
+        console.log('Estimated PI value is: %d\n', estimatedPiValue);
+    }
+}
+
+function* step(squareSize, points) {
+    for (var point of points) {
+        yield {
+            x: point.x,
+            y: point.y,
+            isInCircle: isPointInCircle(point, squareSize)
+        };
+    }
+}
+
+function* take(gen, count) {
+    for (var i = 0; i < count; i++) {
+        yield gen.next().value;
+    }
+}
+
+function* generateRandomPoints(squareSize) {
+    while (true) {
+        yield {
+            x: Math.random(),
+            y: Math.random()
+        }; 
+    }
+}
 
 function isPointInCircle(p, radius) {
     return Math.sqrt(p.x * p.x + p.y * p.y) < radius;
 }
 
-function generateRandomPointInSquare(squareSize) {
-    return {
-        x: Math.floor((Math.random() * squareSize) + 1),
-        y: Math.floor((Math.random() * squareSize) + 1)
-    };
-}
-
-function estimatePi(squareSize, numOfTrials) {
-    var numOfPointsInCircle      = 0;
-    var numOfPointsOutsideCircle = 0;
-
-    for (var i = 0; i < numOfTrials; i++) {
-        if (isPointInCircle(generateRandomPointInSquare(squareSize), squareSize)) {
-            ++numOfPointsInCircle;
-        } else {
-            ++numOfPointsOutsideCircle;
-        }
-    }
-
-    return (numOfPointsInCircle / (numOfPointsInCircle + numOfPointsOutsideCircle)) * 4;
-}
-
-console.log('Estimating PI value using circle inscribed into with square size %d ' +
-    'using %d trials', SquareSideSize, NumberOfTrials);
-
-var pi = estimatePi(SquareSideSize, NumberOfTrials);
-
-console.log('Estimated PI value is: ', pi);
+console.log('Estimating PI value using circle inscribed into with square size %d ', SquareSideSize);
+estimatePi(SquareSideSize);
